2686af758454a1d799e2f4ec5955b1d401aef079
Ahmedhamza <160553755+Hamzii2@users.noreply.github.com>
2026-07-30 02:29:51 +0100

    Create Auth.css

---
+++ Auth.cc
@@ -0,0 +1,35 @@
+body {
+    background:#fff8f0;
+    font-family:Poppins, sans-serif;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    height:100vh;
+}
+
+.auth-box {
+    background:white;
+    padding:40px;
+    width:350px;
+    border-radius:15px;
+    box-shadow:0 10px 25px rgba(0,0,0,0.1);
+    text-align:center;
+}
+
+input {
+    width:100%;
+    padding:15px;
+    margin:10px 0;
+    border-radius:10px;
+    border:1px solid #ddd;
+}
+
+button {
+    width:100%;
+    padding:15px;
+    background:#d4af37;
+    border:none;
+    border-radius:25px;
+    cursor:pointer;
+    font-weight:bold;
+}
